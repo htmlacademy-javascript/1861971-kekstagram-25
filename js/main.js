a03ee754dc94a67d53eebf556e86b22746988a12
Влад <v4nkov.v@yandex.ru>
2022-02-24 17:59:37 +0300

    Рефакторинг функции toFindNumber

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-let minValue;
-let maxValue;
+let minValue = -1;
+let maxValue = 9;
+let messeg ;
 
 function toFindNumber (out, to) {
   if (to > out && out >= 0) {
     return Math.ceil(Math.random() * (to - out)) + out;
   } if (to <= out) { // Условие проверяет что бы параметр 'to' не был больше или равен 'out'. Если условие возвращает 'true'.
-    to = out;        // Параметру 'to' присваивается значение 'out'.
-    out = to;        // Параметру 'out' присваивается значение 'to'.
-    return Math.ceil(Math.random() * (to - out)) + out;
+    //to = out;         Параметру 'to' присваивается значение 'out'.
+    //out = to;         Параметру 'out' присваивается значение 'to'.
+    messeg = 'Запущена 1';
   } if (out < 0) { // Условие проверяет что бы параметр 'out' не был отрицательное число по тех.заданию. Если условие возвращает 'true'.
-    out = 0;       // Параметру 'out' присваивается число '0'.
-    return Math.ceil(Math.random() * (to - out)) + out;
+    // out = 0;        Параметру 'out' присваивается число '0'.
+    messeg = 'Запущена 2';
   }
+  // return Math.ceil(Math.random() * (to - out)) + out;
+  return messeg;
 }
 
-toFindNumber (minValue, maxValue);
-
+let result = toFindNumber (minValue, maxValue);
+console.log (result);
 let string;
 let maxLengthString;
 
