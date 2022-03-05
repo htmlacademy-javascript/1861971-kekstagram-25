d32acfba8da2a7843118edcaf71053257a8443d7
Влад <v4nkov.v@yandex.ru>
2022-03-05 16:40:12 +0300

    Модуль, создает данные.

---
+++ js/data.js
@@ -0,0 +1,58 @@
+import {getRandomArrayElement, toFindNumber} from './util.js';
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const firstName = [
+  'Герман',
+  'Макар',
+  'Злата',
+  'Гриша',
+  'Вадим',
+  'Лёша'
+];
+
+const descriptionPhotos = [
+  'город Волгоград',
+  'город Нижний-Новгород',
+  'город Хабаровск',
+  'город Плесецк',
+  'город Красноярск',
+  'город Дамбай',
+  'город Кисловодск',
+];
+
+const numberOfObjects = 25;
+
+const comments = [];
+
+function buildObject () {
+  return {
+    id: toFindNumber(1, 25),
+    url: `photos/${toFindNumber(1, 25)}.jpg`,
+    description: getRandomArrayElement(descriptionPhotos),
+    likes: toFindNumber(15, 200),
+    comment: getArrObjects()
+  };
+}
+
+function getArrObjects () {
+  for (let i=0; i < numberOfObjects; i++) {
+    comments [i] =  { id: toFindNumber(1, 25),
+      avatar:`img/avatar${toFindNumber(1, 25)}.svg.`,
+      message: getRandomArrayElement(messages),
+      name: getRandomArrayElement(firstName)
+    };
+  }
+  return comments;
+}
+
+const arrayWithObject = Array.from({length:numberOfObjects}, buildObject);
+
+export {arrayWithObject};
