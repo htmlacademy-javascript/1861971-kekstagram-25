e29c9a9f97c05b94e36c557fbe45b4b544ab72b2
Влад <v4nkov.v@yandex.ru>
2022-03-20 11:03:10 +0300

    Показ изображений по клику.

--- js/drawing-thumbnail.js
+++ js/drawing-thumbnail.js
@@ -1,20 +1,27 @@
 import {arrayWithPhotoData} from './data.js';
 
+const socialComments = document.querySelector('.social__comments');
 const template = document.querySelector('#picture').content;
 const templateImage = template.querySelector('.picture');
-
 const elementRenderingBlock = document.querySelector('.pictures');
 
-const fragmentsOfTemplates = document.createDocumentFragment();
 
-arrayWithPhotoData.forEach((value, i) => {
-  const {url,likes,comment}=arrayWithPhotoData[i];
+function addUploadedImages(){
+  const fragmentsOfTemplates = document.createDocumentFragment();
+  arrayWithPhotoData.forEach((value, i) => {
+    const {url,likes,comment}=arrayWithPhotoData[i];
+    const readySample = templateImage.cloneNode(true);
+    readySample.querySelector('.picture__img').src = url;
+    readySample.querySelector('.picture__comments').textContent = comment.length;
+    readySample.querySelector('.picture__likes').textContent = likes;
+    fragmentsOfTemplates.appendChild(readySample);
+  });
+  elementRenderingBlock.appendChild(fragmentsOfTemplates);
+}
 
-  const readySample = templateImage.cloneNode(true);
-  readySample.querySelector('.picture__img').src = url;
-  readySample.querySelector('.picture__comments').textContent = comment.length;
-  readySample.querySelector('.picture__likes').textContent = likes;
-  fragmentsOfTemplates.appendChild(readySample);
-});
 
-elementRenderingBlock.appendChild(fragmentsOfTemplates);
+function removeSocialComments () {
+  socialComments.innerHTML = '';
+}
+
+export{addUploadedImages, removeSocialComments ,socialComments};
