cb1ff80462bc4f8e98649050eebedcc6e0f097c2
Влад <v4nkov.v@yandex.ru>
2022-03-22 14:39:57 +0300

    Вернул в исходную.

--- js/drawing-thumbnail.js
+++ js/drawing-thumbnail.js
@@ -6,22 +6,20 @@ const templateImage = template.querySelector('.picture');
 const elementRenderingBlock = document.querySelector('.pictures');
 
 
-function addUploadedImages(){
-  const fragmentsOfTemplates = document.createDocumentFragment();
-  arrayWithPhotoData.forEach((value, i) => {
-    const {url,likes,comment}=arrayWithPhotoData[i];
-    const readySample = templateImage.cloneNode(true);
-    readySample.querySelector('.picture__img').src = url;
-    readySample.querySelector('.picture__comments').textContent = comment.length;
-    readySample.querySelector('.picture__likes').textContent = likes;
-    fragmentsOfTemplates.appendChild(readySample);
-  });
-  elementRenderingBlock.appendChild(fragmentsOfTemplates);
-}
+const fragmentsOfTemplates = document.createDocumentFragment();
+arrayWithPhotoData.forEach((value, i) => {
+  const {url,likes,comment}=arrayWithPhotoData[i];
+  const readySample = templateImage.cloneNode(true);
+  readySample.querySelector('.picture__img').src = url;
+  readySample.querySelector('.picture__comments').textContent = comment.length;
+  readySample.querySelector('.picture__likes').textContent = likes;
+  fragmentsOfTemplates.appendChild(readySample);
+});
+elementRenderingBlock.appendChild(fragmentsOfTemplates);
 
 
 function removeSocialComments () {
   socialComments.innerHTML = '';
 }
 
-export{addUploadedImages, removeSocialComments ,socialComments};
+export{removeSocialComments, socialComments};
