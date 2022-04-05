1a238e979aefb4da0fe9d41846c519f686c20925
Влад <v4nkov.v@yandex.ru>
2022-04-05 18:34:55 +0300

    Ст.5, подключил модуль

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {createPhotos} from './drawing-thumbnail.js';
 import  './drawing-full-size-image.js';
 import  './form-validation-check.js';
 import   './scale-and-effect.js';
+import './send-success-message-send-failed.js';
 
 fetch('https://25.javascript.pages.academy/kekstagram/data')
   .then((response)=>response.json())
