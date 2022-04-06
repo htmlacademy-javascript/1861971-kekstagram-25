66c18a19f73916932d83b88b6d588f9041986f1b
Влад <v4nkov.v@yandex.ru>
2022-04-06 19:36:10 +0300

    Внес изменения

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@ import  './drawing-full-size-image.js';
 import  './form-validation-check.js';
 import   './scale-and-effect.js';
 import './send-success-message-send-failed.js';
+import {getMessageError} from './util.js';
 
 fetch('https://25.javascript.pages.academy/kekstagram/data')
+  .then((response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      getMessageError();
+    }
+  })
   .then((response)=>response.json())
-  .then((arrayWithPhotoData)=>{
-    createPhotos(arrayWithPhotoData);
-  });
+  .then((arrayWithPhotoData)=>{createPhotos(arrayWithPhotoData);})
+  .catch(() => getMessageError());
