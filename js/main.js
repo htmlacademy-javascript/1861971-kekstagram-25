ed5e8397081df6450b679018c45aa05835ae1235
Влад <v4nkov.v@yandex.ru>
2022-04-12 18:49:29 +0300

    Подключил 'photo-selection.js'

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-import {createPhotos} from './drawing-thumbnail.js';
+import {createPhotos,imageFilter,clickComment,click10randomPhotos,clickphotoInitialOrder} from './drawing-thumbnail.js';
 import  './drawing-full-size-image.js';
 import  './form-validation-check.js';
 import   './scale-and-effect.js';
 import './send-success-message-send-failed.js';
-import {getMessageError} from './util.js';
+import {getMessageError,debounce} from './util.js';
+import './photo-selection.js';
 
 fetch('https://25.javascript.pages.academy/kekstagram/data')
   .then((response) => {
     if (response.ok) {
+      imageFilter.classList.remove('img-filters--inactive');
       return response;
     } else {
       getMessageError();
     }
   })
   .then((response)=>response.json())
-  .then((arrayWithPhotoData)=>{createPhotos(arrayWithPhotoData);})
+  .then((arrayWithPhotoData)=>{createPhotos(arrayWithPhotoData);
+    clickComment(debounce(() => createPhotos(arrayWithPhotoData)));
+    click10randomPhotos(debounce(() => createPhotos(arrayWithPhotoData)));
+    clickphotoInitialOrder(debounce(() => createPhotos(arrayWithPhotoData)));
+  })
   .catch(() => getMessageError());
